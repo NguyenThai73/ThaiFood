fc58ec8478d1e7ddac74092312fcc07f57f134f4
NguyenThai73 <nguyenvanthaind73@gmail.com>
2022-06-01 19:53:48 +0700

    add pj

---
+++ constants/categories.js
@@ -0,0 +1,60 @@
+import { icons } from "."
+export const categoryData = [
+    {
+        id: 1,
+        name: "Cuộn",
+        icon: icons.ch_roll,
+    },
+    {
+        id: 2,
+        name: "Bánh mì\nkẹp thịt",
+        icon: icons.burgers,
+    },
+    {
+        id: 3,
+        name: "Thịt\nnướng",
+        icon: icons.barbq,
+    },
+    {
+        id: 4,
+        name: "Handi",
+        icon: icons.ch_handi,
+    },
+    {
+        id: 5,
+        name: "Karhai",
+        icon: icons.ch_karahi,
+    },
+
+    {
+        id: 6,
+        name: "Cá",
+        icon: icons.fish,
+    },
+    {
+        id: 7,
+        name: "Dal\nSabzi",
+        icon: icons.dal,
+    },
+    {
+        id: 8,
+        name: "Bánh\nmì dẹp",
+        icon: icons.paratha,
+    },
+    {
+        id: 9,
+        name: "Sand\nWiches",
+        icon: icons.sandwich,
+    },
+    {
+        id: 10,
+        name: "Khoai\ntây\nchiên",
+        icon: icons.french_fries,
+    },
+    {
+        id: 11,
+        name: "Đồ\nuống",
+        icon: icons.cold_drinks,
+    },
+
+]
---
+++ constants/database.js
@@ -0,0 +1,2 @@
+const DATABASE_URL = "https://order-food-bs-default-rtdb.firebaseio.com/";
+export default DATABASE_URL;
